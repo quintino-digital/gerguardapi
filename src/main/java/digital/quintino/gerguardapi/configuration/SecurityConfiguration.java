3812c84c02937f67405740731964a4cdf99f06ec
Dysprosium <dysprosium@outlook.com.br>
2022-10-27 11:21:53 -0300

    GERGUARDAPI261002022105955FEATURE

---
+++ src/main/java/digital/quintino/gerguardapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package digital.quintino.gerguardapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.httpBasic().and().authorizeHttpRequests().anyRequest().permitAll();
+	}
+
+}
